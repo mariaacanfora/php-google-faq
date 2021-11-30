496a37eff61ea8835c015dc281a486fe915a1287
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-11-30 17:50:00 +0100

    Added footer and static active on latest nav link

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ $navLinks = [
 ];
 
 $parList = [
-    "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?" => [ 
+    "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?" => [
         "haveSubtitle" => false,
 
         "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
 
         "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
 
-        "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.", 
+        "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
 
         "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
 
@@ -32,31 +32,31 @@ $parList = [
     ],
 
     "Perché il mio account è associato a un paese?" => [
-        "haveSubtitle" => true, 
-        "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",        
-        
+        "haveSubtitle" => true,
+        "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+
         "orderedList" => [
             "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:" => [
                 "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
-    
-                "Google LLC, con sede negli Stati Uniti, per il resto del mondo."
-            ], 
 
-            "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali." => []            
+                "Google LLC, con sede negli Stati Uniti, per il resto del mondo."
+            ],
+
+            "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali." => []
         ],
 
         "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."
 
-    ], 
+    ],
     "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?" => [
-        "haveSubtitle" => false, 
+        "haveSubtitle" => false,
         "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
     ],
 
     "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?" => [
         "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
-        "haveSubtitle" => false, 
-        ]
+        "haveSubtitle" => false,
+    ]
 ];
 
 ?>
@@ -64,6 +64,7 @@ $parList = [
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -81,7 +82,7 @@ $parList = [
     <link rel="stylesheet" href="style.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    
+
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 
 
@@ -90,19 +91,26 @@ $parList = [
 
 <body>
     <header>
-        <nav class="navbar navbar-light bg-light">
+        <nav class="navbar navbar-light bg-light pb-0">
             <div class="container-fluid justify-content-start align-items-baseline">
                 <a href="#" class="navbar-brand pe-1 m-0"> <img src="img/googleLogo.png" alt="google logo"></a>
-                <span class="text-decoration-none text-dark fs-4 flex-grow-1">Privacy e termini</span>
+                <span class="text-decoration-none text-secondary fs-4 flex-grow-1">Privacy e termini</span>
                 <span class="rounded-circle bg-secondary user-badge text-light">M</span>
             </div>
 
             <div class="container-fluid">
                 <ul class="p-0 m-0">
-                    <?php 
-                        foreach ($navLinks as $link) {
+                    <?php
+                    foreach ($navLinks as $k => $link) {
+
+                        if ($k === (count($navLinks) - 1)) {
+                            echo "<li class='d-inline-block border-bottom border-5 border-primary'><a href='#' class='text-decoration-none text-dark'> $link </a></li>";
+                        } else {
                             echo "<li class='d-inline-block pe-5'><a href='#' class='text-decoration-none text-dark'> $link </a></li>";
-                        };
+                        }
+
+
+                    };
                     ?>
                 </ul>
             </div>
@@ -112,51 +120,73 @@ $parList = [
     <main>
         <div class="container">
             <?php
-         
-                foreach ($parList as $key => $item) {
 
-                    echo "<div class='mb-5'>";
-                    echo "<h4> $key </h4>";
+            foreach ($parList as $key => $item) {
 
-                    if ($parList[$key]["haveSubtitle"]) {
-                        echo $parList[$key][0];
-                        /* echo "<pre>";
+                echo "<div class='mb-5'>";
+                echo "<h4> $key </h4>";
+
+                if ($parList[$key]["haveSubtitle"]) {
+                    echo $parList[$key][0];
+                    /* echo "<pre>";
                         var_dump($parList[$key][0]);
                         echo "</pre>"; */
-                        echo "<ol>";
-                            foreach ($parList[$key]['orderedList'] as $j => $par) {                             
-                                echo "<li>";
+                    echo "<ol>";
+                    foreach ($parList[$key]['orderedList'] as $j => $par) {
+                        echo "<li>";
 
-                                    /* echo "<pre>";
+                        /* echo "<pre>";
                                     var_dump($parList[$key]['orderedList']);
                                     echo "</pre>"; */
 
-                                    echo $j;
-                                    if (!empty($parList[$key]['orderedList'][$j])) {
-                                        echo "<ul>";
-                                        foreach ($parList[$key]['orderedList'][$j] as $value) {
-                                            echo "<li>";
-                                            echo $value;
-                                            echo "</li>";
-                                        }
-                                        echo "</ul>";                                
-                                    }
-                                echo "</li>";                                    
+                        echo $j;
+                        if (!empty($parList[$key]['orderedList'][$j])) {
+                            echo "<ul>";
+                            foreach ($parList[$key]['orderedList'][$j] as $value) {
+                                echo "<li>";
+                                echo $value;
+                                echo "</li>";
                             }
-                        echo "</ol>"; 
-                        echo $parList[$key][1];
-                    } else {
-                        foreach ($parList[$key] as $par) {
-                            if (!empty($par)) {
-                                # code...
-                                echo "<p> $par </p>";
-                            }
+                            echo "</ul>";
+                        }
+                        echo "</li>";
+                    }
+                    echo "</ol>";
+                    echo $parList[$key][1];
+                } else {
+                    foreach ($parList[$key] as $par) {
+                        if (!empty($par)) {
+                            # code...
+                            echo "<p> $par </p>";
                         }
                     }
-                    echo "</div>";
                 }
+                echo "</div>";
+            }
             ?>
         </div>
     </main>
+
+    <footer class="bg-lt-grey py-3">
+        <div class="container  d-flex justify-content-between">
+            <ul class="m-0 p-0 flex-grow-1">
+                <li class="d-inline-block pe-2"><a href="#">Google</a></li>
+                <li class="d-inline-block pe-2"><a href="#">Tutto su Google</a></li>
+                <li class="d-inline-block pe-2"><a href="#">Privacy</a></li>
+                <li class="d-inline-block pe-2"><a href="#">Termini</a></li>
+            </ul>
+
+            <div style="width: 15%;" class="d-flex align-items-center">
+                <i class="fas fa-globe-africa pe-2 text-primary"></i>
+                <select class="form-select border-dark" aria-label="Default select example" >
+                    <option value="1">Italiano</option>
+                    <option value="1">English</option>
+                    <option value="1">Espanol</option>
+                    <option value="1">Francais</option>
+                </select>
+            </div>
+        </div>
+    </footer>
 </body>
+
 </html>
