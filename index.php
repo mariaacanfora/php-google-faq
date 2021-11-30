e28399c5ab9baab9e53a4eac7ca7a5aeef9ac26e
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-11-30 17:15:36 +0100

    Added all paragraphs

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ $navLinks = [
 
 $parList = [
     "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?" => [ 
+        "haveSubtitle" => false,
+
         "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
 
         "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
@@ -18,6 +20,8 @@ $parList = [
     ],
 
     "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?" => [
+        "haveSubtitle" => false,
+
         "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
 
         "Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
@@ -27,13 +31,32 @@ $parList = [
         "Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
     ],
 
-/*     "Perché il mio account è associato a un paese?" => [
-        "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+    "Perché il mio account è associato a un paese?" => [
+        "haveSubtitle" => true, 
+        "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",        
+        
+        "orderedList" => [
+            "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:" => [
+                "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
+    
+                "Google LLC, con sede negli Stati Uniti, per il resto del mondo."
+            ], 
 
-        ""
+            "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali." => []            
+        ],
+
+        "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."
+
     ], 
-    "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
-    "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?" */
+    "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?" => [
+        "haveSubtitle" => false, 
+        "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
+    ],
+
+    "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?" => [
+        "haveSubtitle" => false, 
+        "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
+    ]
 ];
 
 ?>
@@ -88,19 +111,43 @@ $parList = [
 
     <main>
         <div class="container">
-            <?php 
+            <?php
+         
                 foreach ($parList as $key => $item) {
+
+                    echo "<div class='mb-5'>";
                     echo "<h4> $key </h4>";
-                    
-                    /* echo "<pre>";
-                    var_dump($parList[$key]);
-                    echo "</pre>"; */
 
-                    foreach ($parList[$key] as $par) {
-                        echo "<p> $par </p>";
+                    if ($parList[$key]["haveSubtitle"]) {
+                        echo "<ol>";
+                            foreach ($parList[$key]['orderedList'] as $j => $par) {                             
+                                echo "<li>";
+
+                                    /* echo "<pre>";
+                                    var_dump($parList[$key]['orderedList']);
+                                    echo "</pre>"; */
+
+                                    echo $j;
+                                    if (!empty($parList[$key]['orderedList'][$j])) {
+                                        echo "<ul>";
+                                        foreach ($parList[$key]['orderedList'][$j] as $value) {
+                                            echo "<li>";
+                                            echo $value;
+                                            echo "</li>";
+                                        }
+                                        echo "</ul>";                                
+                                    }
+                                echo "</li>";                                    
+                            }
+                        echo "</ol>"; 
+                    } else {
+                        foreach ($parList[$key] as $par) {
+                            echo "<p> $par </p>";
+                        }
                     }
-                    
 
+                    
+                    echo "</div>";
                 }
             ?>
         </div>
